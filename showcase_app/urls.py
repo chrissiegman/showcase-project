bd81817442f3761df35dc88f487937f8f6d32fb5
chris <chrissiegman@yahoo.com>
2015-10-12 16:14:40 -0500

    Added simple url mappings and BASIC views.

--- showcase_app/urls.py
+++ showcase_app/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import patterns, url
 from showcase_app import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'))
+                       url(r'^$', views.index, name='index'),
+                       url(r'^about/', views.about, name='about'),
+                       url(r'^contact/', views.contact, name='contact'),
+                       url(r'^projects/', views.projects, name='projects'),
+                       url(r'^resumee/', views.resumee, name='resumee'),
+                       )
--- showcase_app/views.py
+++ showcase_app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("Hello World.")
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def projects(request):
+    return HttpResponse("This is the projects page.")
+
+def resumee(request):
+    return HttpResponse("This is the resumee page.")
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
