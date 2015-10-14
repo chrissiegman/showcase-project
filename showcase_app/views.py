c74364770831d381d5f70b118fd1f74683301a3b
chris <chrissiegman@yahoo.com>
2015-10-14 14:24:05 -0500

    Added functionality to contact page.

--- showcase_app/views.py
+++ showcase_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from showcase_app.forms import ContactForm
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.template import Context
 
 def index(request):
     return render(request, 'showcase_app/index.html', {})
@@ -15,4 +18,34 @@ def resumee(request):
 
 def contact(request):
     form_class = ContactForm
-    return render(request, 'showcase_app/contact.html', {'form': form_class})
+
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            message = request.POST.get('message', '')
+
+            template = get_template('contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'message': message
+            })
+
+            content = template.render(context)
+
+            email = EmailMessage(
+                'New contact form submission',
+                content,
+                "Your website" +'<hi@weddinglovely.com>',
+                ['youremail@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return HttpResponse('Email Sent')
+        else:
+            return HttpResponse('Error: form submission invalid.')
+    else:
+        return render(request, 'showcase_app/contact.html', {'form': form_class})
