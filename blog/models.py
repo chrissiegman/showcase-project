f495f0e3229933eb495b6bdadd48bc91414c77e6
chris <chrissiegman@yahoo.com>
2015-10-15 15:43:52 -0500

    Added simple Post model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=10000)
+    pub_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=100)
