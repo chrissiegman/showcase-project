bbb50ebb2edc1dc52d11550a9095bbaf5011af29
chris <chrissiegman@yahoo.com>
2015-10-17 15:02:52 -0500

    Removed debugging code from twitterbot

--- biebfeed/twitterbot.py
+++ biebfeed/twitterbot.py
@@ -12,14 +12,6 @@ def print_user_info(screen_name):
     print("Screen name: ", user.screen_name)
     print("Number of followers: ", user.followers_count)
 
-# will only work if tweet_number <= 20
-def get_user_tweet(screen_name):
-    tweet = api.user_timeline(screen_name)
-    print(type(tweet))
-    print(type(tweet[0].text))
-    print(tweet[0].text)
-    return(tweet[0].text)
-
 def get_user_tweets(screen_name):
     tweets = api.user_timeline(screen_name)
 
@@ -31,14 +23,6 @@ def get_user_tweets(screen_name):
     for i in range(0, 5):
         text_list.append(tweets[i].text)
 
-    print(type(key_list))
-    print(key_list)
-
-    print(type(text_list))
-    print(text_list)
-
     text_dict = dict(zip(key_list, text_list))
-    print(type(text_dict))
-    print(text_dict)
 
     return(text_dict)
