4f7ff402dbd4a0e5e0fb91c97f6d4761206a645b
chris <chrissiegman@yahoo.com>
2015-10-21 18:41:17 -0500

    Finishing touches on biebfeed. Now need LIST CONTROL for users

--- biebfeed/views.py
+++ biebfeed/views.py
@@ -5,15 +5,22 @@ from biebfeed.models import TwitterTarget
 
 def index(request):
     current_user = request.user
-    twitter_targets = TwitterTarget.objects.filter(user__username__contains=current_user)
-
-    master_tweet_dict = {}
-    for target in twitter_targets:
-        master_tweet_dict[target.target_username] = twitterbot.get_user_tweets(target.target_username)
-
     context_dict = {}
-    context_dict['twitter_targets'] = twitter_targets
-    context_dict['master_tweet_dict'] = master_tweet_dict
+    master_tweet_dict = {}
 
-    print(master_tweet_dict)
+    if str(current_user) != 'AnonymousUser':
+
+        print("current user is:" + str(current_user))
+
+        twitter_targets = TwitterTarget.objects.filter(user__username__contains=current_user)
+
+        for target in twitter_targets:
+            master_tweet_dict[target.target_username] = twitterbot.get_user_tweets(target.target_username)
+
+        context_dict['master_tweet_dict'] = master_tweet_dict
+
+    else:
+        master_tweet_dict['justinbieber'] = twitterbot.get_user_tweets('justinbieber')
+        context_dict['master_tweet_dict'] = master_tweet_dict
+
     return render(request, 'biebfeed/index.html', context_dict)
