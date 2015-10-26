6c61190b499c088fc368602a0db6a2dd818b29a9
chris <chrissiegman@yahoo.com>
2015-10-26 01:17:48 -0500

    First unit test uses TestCasewq

--- showcase_app/tests.py
+++ showcase_app/tests.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from showcase_app.views import index 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
-
+class IndexPageTest(TestCase):
+    
+    def test_root_url_resolves_to_index_page_view(self):
+        found = resolve('/showcase/')
+        self.assertEqual(found.func, index)
