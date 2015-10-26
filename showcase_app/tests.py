c24fea340708f8e5dc8efbc0250fa10ea4d161d4
chris <chrissiegman@yahoo.com>
2015-10-26 01:50:54 -0500

    Unit tests for Index and projects page

--- showcase_app/tests.py
+++ showcase_app/tests.py
@@ -1,9 +1,21 @@
-from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 from django.test import TestCase
-from showcase_app.views import index 
+from django.http import HttpRequest
+from showcase_app.views import index, projects, contact
+
 
 class IndexPageTest(TestCase):
     
     def test_root_url_resolves_to_index_page_view(self):
-        found = resolve('/showcase/')
-        self.assertEqual(found.func, index)
+        request = HttpRequest()
+        response = index(request)
+        expected_html = render_to_string('showcase_app/index.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+class ProjectsPageTest(TestCase):
+
+    def test_projects_url_resolves_to_projects_page_view(self):
+        request = HttpRequest()
+        response = projects(request)
+        expected_html = render_to_string('showcase_app/projects.html')
+        self.assertEqual(response.content.decode(), expected_html)
